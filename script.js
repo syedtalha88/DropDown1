9924ff1ad076f3cc09da95783420b18930229006
syedtalha88 <150226683+syedtalha88@users.noreply.github.com>
2023-11-09 20:35:55 +0530

    DropDown1

---
+++ script.js
@@ -0,0 +1,6 @@
+const Containerjs=document.getElementById('container-id');
+const buttonjs=document.getElementById('profile-id');
+
+buttonjs.addEventListener('click',()=>{
+    Containerjs.classList.toggle('show-dropmenu');
+})
